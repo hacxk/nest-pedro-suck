9501ec73070fdcd3e9c391017fc42d62640d29cb
hacxk <zaidzeenathe@gmail.com>
2024-07-25 18:15:32 +0530

    Pedro Look New Updates!

--- src/module/whatsapp/whatsapp.controller.ts
+++ src/module/whatsapp/whatsapp.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Get, Param, Res, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Get, Param, Query, Res, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
 import { instanceDto } from './dto/instanceDto';
 import { JwtService } from '@nestjs/jwt';
@@ -11,7 +11,7 @@ export class WhatsappController {
         private readonly whatsappService: WhatsappService,
         private readonly jwtService: JwtService,
         private readonly prisma: PrismaService
-    ) {}
+    ) { }
 
     @Post('auth')
     @HttpCode(HttpStatus.OK)
@@ -20,13 +20,14 @@ export class WhatsappController {
             const payload = await this.jwtService.verifyAsync(instanceData.token, {
                 secret: process.env.JWT_SECRET
             });
-
             const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
             if (!user) throw new UnauthorizedException();
-
-            const { qrCodeDataUrl, success } = await this.whatsappService.getQrCode({ email: user.email }); // Use user's email from DB
-            
-            res.status(success ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR).json({ qrCodeDataUrl, message: success ? '' : 'Failed to generate QR code' });
+            const { qrCodeDataUrl, success, message } = await this.whatsappService.getQrCode({ email: user.email }); // Use user's email from DB
+            if (message) {
+                res.status(HttpStatus.OK).json({ message, success });
+            } else {
+                res.status(success ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR).json({ qrCodeDataUrl, message: success ? '' : 'Failed to generate QR code' });
+            }
         } catch (error) {
             res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Invalid token' });
         }
@@ -34,12 +35,37 @@ export class WhatsappController {
 
     @Get('auth/status/:email')
     async getConnectionStatus(@Param('email') email: string) {
-        const instance = await this.prisma.instance.findFirst({ 
-            where: { user: { email } }, 
-            include: { user: true } // Include the user object to access email
+        const user = await this.prisma.user.findUnique({
+            where: { email }
         });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        const instance = await this.prisma.instance.findFirst({
+            where: { userId: user.id },
+            include: { user: true }
+        });
+        if (!instance) {
+            throw new NotFoundException('Instance not found');
+        }
+        return { status: instance.status };
+    }
 
-        if (!instance) throw new NotFoundException('Instance not found');
-        return { status: instance };
+    @Post('instance-close')
+    @HttpCode(HttpStatus.OK)
+    async closeSocket(@Body() { email, token }: { email: string; token: string }, @Res() res: Response) {
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: process.env.JWT_SECRET
+            });
+            const user = await this.prisma.user.findUnique({ where: { email } });
+            if (!user || user.id !== payload.sub) {
+                throw new UnauthorizedException();
+            }
+            await this.whatsappService.closeSocket(email); // Use the public method
+            res.status(HttpStatus.OK).json({ message: 'Socket closed successfully.' });
+        } catch (error) {
+            res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Invalid token or socket closure failed.' });
+        }
     }
 }
--- src/module/whatsapp/whatsapp.service.ts
+++ src/module/whatsapp/whatsapp.service.ts
@@ -1,40 +1,57 @@
-import { Injectable, Logger } from '@nestjs/common';
-import makeWASocket, { ConnectionState, WASocket, DisconnectReason, BaileysEventEmitter, fetchLatestBaileysVersion } from '@whiskeysockets/baileys';
+import { Injectable, Logger, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import makeWASocket, { ConnectionState, WASocket, DisconnectReason, BaileysEventEmitter } from '@whiskeysockets/baileys';
 import { useRedisAuthState } from 'redis-baileys';
 import { ConfigService } from '@nestjs/config';
-import qrcode from 'qrcode';
-import Boom from '@hapi/boom';
+import * as qrcode from 'qrcode';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { Instance } from '@prisma/client';
 
-interface GetQrCodeParams { email: string; }
+interface GetQrCodeParams {
+    email: string;
+}
 
 @Injectable()
 export class WhatsappService {
-    private sockets: Map<string, WASocket> = new Map();
-    private qrCodes: Map<string, { qr: string, timestamp: number }> = new Map();
-    private eventEmitters: Map<string, BaileysEventEmitter> = new Map();
+    private sockets = new Map<string, WASocket>();
+    private qrCodes = new Map<string, { qr: string; timestamp: number }>();
+    private eventEmitters = new Map<string, BaileysEventEmitter>();
+    private socketStatus = new Map<string, 'online' | 'offline'>();
+    private explicitlyClosedSockets = new Set<string>();
 
     private readonly redisConfig = {
         password: this.configService.get<string>('REDIS_PASSWORD'),
         host: this.configService.get<string>('REDIS_HOST'),
-        port: this.configService.get<number>('REDIS_PORT', 6379),
+        port: this.configService.get<number>('REDIS_PORT'),
     };
 
     private readonly logger = new Logger(WhatsappService.name);
 
-    constructor(private readonly configService: ConfigService, private prisma: PrismaService) { }
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly prisma: PrismaService
+    ) {}
 
-    private async createSocket(email: string): Promise<WASocket> {
-        const { version } = await fetchLatestBaileysVersion();
+    private async createSocket(email: string): Promise<WASocket | string> {
+        if (this.sockets.has(email) && this.socketStatus.get(email) === 'online') {
+            return 'Already Socket Created!';
+        }
+
         const { state, saveCreds } = await this.getAuthState(email);
+        const socket = makeWASocket({
+            auth: state,
+            printQRInTerminal: true,
+            mobile: false,
+        });
 
-        const socket = makeWASocket({ auth: state, printQRInTerminal: false, mobile: false, version });
-
+        await this.createInstanceForUser(email, socket.user.id);
         this.sockets.set(email, socket);
         this.eventEmitters.set(email, socket.ev);
+        this.socketStatus.set(email, 'offline');
 
         socket.ev.on('creds.update', saveCreds);
-        socket.ev.on('connection.update', (update: Partial<ConnectionState>) => this.handleConnectionUpdate(email, update));
+        socket.ev.on('connection.update', (update: Partial<ConnectionState>) =>
+            this.handleConnectionUpdate(email, update)
+        );
 
         return socket;
     }
@@ -47,58 +64,177 @@ export class WhatsappService {
         const user = await this.prisma.user.findUnique({ where: { email } });
         if (!user) return;
 
+        const instance = await this.prisma.instance.findFirst({ where: { userId: user.id } });
+        if (!instance) return;
+
         if (update.qr) {
             const qrCodeDataUrl = await qrcode.toDataURL(update.qr);
             this.qrCodes.set(email, { qr: qrCodeDataUrl, timestamp: Date.now() });
         }
 
         if (update.connection === 'close') {
-            if (this.shouldReconnect(update.lastDisconnect)) {
-                this.reconnectSocket(email);
+            const shouldReconnect = 
+                (update.lastDisconnect?.error as any)?.output?.statusCode !== DisconnectReason.loggedOut
+                && !this.explicitlyClosedSockets.has(email);
+
+            if (shouldReconnect) {
+                await this.reconnectSocket(email);
+            }
+
+            await this.updateInstanceStatus(email, 'offline');
+            this.socketStatus.set(email, 'offline');
+        } else if (update.connection === 'open') {
+            this.logger.log(`Opened connection for ${email}`);
+            await this.updateInstanceStatus(email, 'online');
+            this.socketStatus.set(email, 'online');
+        }
+    }
+
+    private async updateInstanceStatus(email: string, status: 'online' | 'offline') {
+        const user = await this.prisma.user.findUnique({ where: { email } });
+        if (!user) throw new NotFoundException('User not found');
+
+        const instance = await this.prisma.instance.findFirst({ where: { userId: user.id } });
+        if (!instance) throw new NotFoundException('Instance not found');
+
+        await this.prisma.instance.update({
+            where: { id: instance.id },
+            data: { status },
+        });
+    }
+
+    private async reconnectSocket(email: string) {
+        this.logger.log(`Reconnecting socket for ${email}...`);
+        const socket = this.sockets.get(email);
+        if (!socket) {
+            throw new NotFoundException(`No socket found for ${email}. Cannot reconnect.`);
+        }
+
+        try {
+            await socket.end(new Error(`Reconnecting socket for ${email}...`));
+            this.sockets.delete(email);
+            this.eventEmitters.delete(email);
+            this.socketStatus.set(email, 'offline');
+            await this.createSocket(email);
+            this.logger.log(`Reconnected socket for ${email}`);
+        } catch (error) {
+            this.logger.error(`Failed to reconnect socket for ${email}`, error);
+            throw new InternalServerErrorException('Failed to reconnect socket', { cause: error });
+        }
+    }
+
+    async getQrCode(params: GetQrCodeParams): Promise<{ qrCodeDataUrl?: string; success: boolean; message: string }> {
+        const { email } = params;
+        const qrData = this.qrCodes.get(email);
+
+        if (qrData && Date.now() - qrData.timestamp < 60000) {
+            return { qrCodeDataUrl: qrData.qr, success: true, message: "QR Code already generated!" };
+        }
+
+        try {
+            const existingSocketResponse = await this.createSocket(email);
+            if (typeof existingSocketResponse === 'string') {
+                return { qrCodeDataUrl: undefined, success: false, message: existingSocketResponse };
+            }
+
+            const socket = existingSocketResponse;
+            let qrCodeDataUrl: string | undefined;
+            let connectionOpened = false;
+
+            const handleQr = async (update: Partial<ConnectionState>) => {
+                if (update.qr) {
+                    try {
+                        qrCodeDataUrl = await qrcode.toDataURL(update.qr);
+                        socket.ev.off('connection.update', handleQr);
+                    } catch (err) {
+                        this.logger.error('Failed to generate QR code data URL.', err);
+                    }
+                } else if (update.connection === 'open') {
+                    connectionOpened = true;
+                    await this.updateInstanceStatus(email, 'online');
+                    socket.ev.off('connection.update', handleQr);
+                }
+            };
+
+            socket.ev.on('connection.update', handleQr);
+
+            const response = await new Promise<{ qrCodeDataUrl?: string; success: boolean; message: string }>(
+                (resolve, reject) => {
+                    const timeout = setTimeout(() => {
+                        socket.ev.off('connection.update', handleQr);
+                        if (!qrCodeDataUrl && !connectionOpened) {
+                            reject(new Error('Failed to receive QR code and connection not opened.'));
+                        } else {
+                            resolve({
+                                qrCodeDataUrl,
+                                success: true,
+                                message: qrCodeDataUrl ? "QR code generated successfully!" : "Connection opened successfully!",
+                            });
+                        }
+                    }, 60000);
+                }
+            );
+
+            return response;
+
+        } catch (error) {
+            this.logger.error('Failed to initialize socket or handle QR code.', error);
+            throw new InternalServerErrorException('Failed to initialize socket or handle QR code.', { cause: error });
+        }
+    }
+
+    private async createInstanceForUser(email: string, number: string): Promise<Instance> {
+        const user = await this.prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        let instance = await this.prisma.instance.findFirst({ where: { userId: user.id } });
+        if (instance) {
+            return instance;
+        }
+        try {
+            return await this.prisma.instance.create({
+                data: {
+                    phone: number.split('@s.whatsapp.net')[0],
+                    username: `instance_${user.id}`,
+                    status: 'offline',
+                    user: { connect: { id: user.id } },
+                },
+            });
+        } catch (error) {
+            if (error.code === 'P2002') {
+                return null;
+            } else {
+                throw error;
             }
         }
     }
 
-    private shouldReconnect(lastDisconnect?: { error?: Error; date: Date }) {
-        if (lastDisconnect && lastDisconnect.error) {
-            const errorReason = lastDisconnect.error.name as unknown as DisconnectReason;
-            return ![DisconnectReason.loggedOut, DisconnectReason.restartRequired].includes(errorReason);
-        }
-        return true;
-    }
-
-    private async reconnectSocket(email: string) {
+    async closeSocket(email: string): Promise<void> {
         const socket = this.sockets.get(email);
-        if (socket) {
-            await socket.logout();
-            await this.createSocket(email);
-        } else {
-            throw Boom.notFound(`No socket found for ${email}. Cannot reconnect.`);
-        }
-    }
-
-    async getQrCode(params: GetQrCodeParams): Promise<{ qrCodeDataUrl: string, success: boolean }> {
-        const qrData = this.qrCodes.get(params.email);
-        if (qrData && Date.now() - qrData.timestamp < 60000) { // QR code valid for 1 minute
-            return { qrCodeDataUrl: qrData.qr, success: true };
+        if (!socket) {
+            throw new NotFoundException(`No socket found for ${email}. Cannot close.`);
         }
 
         try {
-            await this.createSocket(params.email);
-            const { qrCodeDataUrl, success } = await new Promise<{ qrCodeDataUrl: string, success: boolean }>((resolve) => {
-                this.eventEmitters.get(params.email)?.on('connection.update', (update: Partial<ConnectionState>) => {
-                    if (update.qr) {
-                        qrcode.toDataURL(update.qr).then(qrCodeDataUrl => resolve({ qrCodeDataUrl, success: true }));
-                    } else {
-                        resolve({ qrCodeDataUrl: '', success: false });
-                    }
-                });
-            });
-            return { qrCodeDataUrl, success };
-
+            await socket.end(new Error(`Closing socket for ${email}...`));
+            this.sockets.delete(email);
+            this.eventEmitters.delete(email);
+            this.socketStatus.set(email, 'offline');
+            this.explicitlyClosedSockets.add(email);
+            const user = await this.prisma.user.findUnique({ where: { email } });
+            if (user) {
+                const instance = await this.prisma.instance.findFirst({ where: { userId: user.id } });
+                if (instance) {
+                    await this.prisma.instance.update({
+                        where: { id: instance.id },
+                        data: { status: 'offline' },
+                    });
+                }
+            }
         } catch (error) {
-            this.logger.error('Failed to initialize socket or handle QR code.', error);
-            throw Boom.internal('Failed to initialize socket or handle QR code.', error);
+            this.logger.error(`Failed to close socket for ${email}`, error);
+            throw new InternalServerErrorException('Failed to close socket', { cause: error });
         }
     }
 }
